e43a2865bd8f2d206a4936f4b912c7dbf3a0165c
blevin72 <135919677+blevin72@users.noreply.github.com>
2023-11-22 08:26:58 -0800

    Finished inventory tutorial

--- Assets/Scripts/InventoryScripts/ItemData.cs
+++ Assets/Scripts/InventoryScripts/ItemData.cs
@@ -6,6 +6,7 @@ public class ItemData : ScriptableObject
     //setting the size of the item icon
     public int width = 1;
     public int height = 1;
+    public int ID = 0;
 
     public Sprite itemIcon;
 }
