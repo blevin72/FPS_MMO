9c22016a803f4bcd09f8da6c575e392506ac4117
blevin72 <135919677+blevin72@users.noreply.github.com>
2024-01-18 13:17:18 -0800

    Character Customization Scripts

---
+++ Assets/ChangePants.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ChangePants : MonoBehaviour
+{
+    public Renderer dressPants;
+    public Renderer shorts;
+    public Renderer joggers;
+}
--- Assets/Scripts/Database_Scripts/DB_Manager.cs
+++ Assets/Scripts/Database_Scripts/DB_Manager.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public static class DB_Manager
 {
     public static string email;
-    public static int experience;
+<<<<<<< HEAD
+    //public static int experience;
+=======
+>>>>>>> af324c0768c6974e98be73fe5f3f89bd0be906c9
 
     public static bool LoggedIn { get { return email != null; } }
 
